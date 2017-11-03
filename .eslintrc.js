f5c66bbb7d64c086062b5f019e8778302bc274d0
Jeff <rustyangel@gmail.com>
2017-11-03 15:08:10 -0600

    📦 Update eslint plugin & settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,21 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 8,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:ember-suave/recommended'
-  ],
   env: {
-    'browser': true
+    browser: true,
   },
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   rules: {
-  }
+    // Code style.
+    semi: ['error', 'always'],
+
+    // Custom Ember rules.
+    'ember/new-module-imports': 'warn',
+    'ember/no-attrs-in-components': 'warn',
+    'ember/no-duplicate-dependent-keys': 'warn',
+    'ember/no-global-jquery': 'warn',
+    'ember/require-super-in-init': 'warn',
+  },
 };
