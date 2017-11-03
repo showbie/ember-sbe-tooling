09bc4f2020f8294156e449e8b255ea397d87dab5
Jeff <rustyangel@gmail.com>
2017-11-03 15:08:42 -0600

    ⚙️ Update blueprint & CI settings to use new eslint plugin

--- blueprints/showbie-tooling/index.js
+++ blueprints/showbie-tooling/index.js
@@ -1,27 +1,29 @@
 /* eslint-env node */
 module.exports = {
-  description: 'Install Showbie code style enforcement configuration and tooling',
+  description:
+    'Install Showbie code style enforcement configuration and tooling',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   beforeInstall() {
+    // eslint-disable-next-line ember/named-functions-in-promises
     return this.addAddonsToProject({
       packages: [
         { name: 'ember-cli-eslint', target: '~3.0.0' },
-        { name: 'ember-cli-code-coverage', target: '~0.3.4' }
-      ]
-    })
-    .then(() => {
+        { name: 'ember-cli-code-coverage', target: '~0.3.4' },
+      ],
+    }).then(() => {
       return this.addPackagesToProject([
-        { name: 'eslint-plugin-ember-suave', target: '~1.0.0' },
-        { name: 'lodash.samplesize', target: '^4.2.0' }
+        { name: 'eslint-plugin-ember', target: '^4.5.0' },
+        { name: 'lodash.samplesize', target: '^4.2.0' },
       ]);
     });
   },
 
   afterInstall() {
     return this.removePackagesFromProject([
-      { name: 'ember-suave' }
+      { name: 'ember-suave' },
+      { name: 'eslint-plugin-ember-suave' },
     ]);
-  }
+  },
 };
