6f4a151b0ce8779c4b371672b6075fef9da50e65
Jeff <rustyangel@gmail.com>
2017-11-08 14:58:42 -0700

    ⚙️ Update eslint setup (#20)
    * 📦 Update eslint plugin & settings
    
    * ⚙️ Update blueprint & CI settings to use new eslint plugin
    
    * ⚙️ Flag ember rules as errors instead of warnings
    
    Until we can determine what how loud this is going to make eslint on existing addons/components.
    
    * 📦 Update lockfile
    
    * 👕 Ignore named-function-in-promise rule

--- blueprints/showbie-tooling/files/.eslintrc.js
+++ blueprints/showbie-tooling/files/.eslintrc.js
@@ -2,15 +2,21 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 8,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:ember-suave/recommended'
-  ],
   env: {
-    'browser': true
+    browser: true,
   },
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   rules: {
-  }
+    // Code style.
+    semi: ['error', 'always'],
+
+    // Custom Ember rules.
+    'ember/new-module-imports': 'error',
+    'ember/no-attrs-in-components': 'error',
+    'ember/no-duplicate-dependent-keys': 'error',
+    'ember/no-global-jquery': 'error',
+    'ember/require-super-in-init': 'error',
+  },
 };
