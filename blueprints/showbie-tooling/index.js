36a3e1db8ebde000c19be1368de721d41dfbd243
Eli Dupuis <eli@showbie.com>
2017-11-16 11:09:19 -0800

    Clean up blueprint

--- blueprints/showbie-tooling/index.js
+++ blueprints/showbie-tooling/index.js
@@ -5,25 +5,23 @@ module.exports = {
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
-  beforeInstall() {
-    // eslint-disable-next-line ember/named-functions-in-promises
-    return this.addAddonsToProject({
-      packages: [
-        { name: 'ember-cli-eslint', target: '~3.0.0' },
-        { name: 'ember-cli-code-coverage', target: '~0.3.4' },
-      ],
-    }).then(() => {
-      return this.addPackagesToProject([
-        { name: 'eslint-plugin-ember', target: '^4.5.0' },
-        { name: 'lodash.samplesize', target: '^4.2.0' },
-      ]);
-    });
-  },
-
   afterInstall() {
+    // eslint-disable-next-line ember/named-functions-in-promises
     return this.removePackagesFromProject([
       { name: 'ember-suave' },
       { name: 'eslint-plugin-ember-suave' },
-    ]);
-  },
+    ]).then(() => {
+      return this.addPackagesToProject([
+        { name: 'eslint-plugin-ember', target: '^4.5.0' },
+        { name: 'lodash.samplesize', target: '~4.2.0' }, // strictly for config/release
+      ]);
+    }).then(() => {
+      return this.addAddonsToProject({
+        packages: [
+          { name: 'ember-cli-eslint', target: '^4.0.0' },
+          { name: 'ember-cli-code-coverage', target: '~0.3.4' },
+        ],
+      });
+    });
+  }
 };
