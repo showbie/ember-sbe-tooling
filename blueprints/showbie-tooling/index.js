a23280aef809901aef470f40a493dab019a01406
Jeff <rustyangel@gmail.com>
2017-11-17 09:16:23 -0700

    🔨 Update stylelint config version in blueprint

--- blueprints/showbie-tooling/index.js
+++ blueprints/showbie-tooling/index.js
@@ -10,27 +10,29 @@ module.exports = {
     return this.removePackagesFromProject([
       { name: 'ember-suave' },
       { name: 'eslint-plugin-ember-suave' },
-    ]).then(() => {
-      return this.addPackagesToProject([
-        { name: 'eslint-plugin-ember', target: '^4.5.0' },
-        { name: 'lodash.samplesize', target: '~4.2.0' }, // strictly for config/release
-        { name: 'stylelint', target: '~8.2.0' },
-        {
-          name: 'stylelint-config-property-sort-order-smacss',
-          target: '~2.0.0',
-        },
-        { name: 'stylelint-config-sass-guidelines', target: '~3.1.0' },
-        { name: 'stylelint-order', target: '^0.7.0' },
-        { name: 'stylelint-scss', target: '~2.1.0' },
-      ]);
-    }).then(() => {
-      return this.addAddonsToProject({
-        packages: [
-          { name: 'ember-cli-eslint', target: '^4.0.0' },
-          { name: 'ember-cli-code-coverage', target: '~0.3.4' },
-          { name: 'ember-cli-template-lint', target: '~0.7.5' },
-        ],
+    ])
+      .then(() => {
+        return this.addPackagesToProject([
+          { name: 'eslint-plugin-ember', target: '^4.5.0' },
+          { name: 'lodash.samplesize', target: '~4.2.0' }, // strictly for config/release
+          { name: 'stylelint', target: '~8.2.0' },
+          {
+            name: 'stylelint-config-property-sort-order-smacss',
+            target: '~2.0.0',
+          },
+          { name: 'stylelint-config-sass-guidelines', target: '~4.0.1' },
+          { name: 'stylelint-order', target: '^0.7.0' },
+          { name: 'stylelint-scss', target: '~2.1.0' },
+        ]);
+      })
+      .then(() => {
+        return this.addAddonsToProject({
+          packages: [
+            { name: 'ember-cli-eslint', target: '^4.0.0' },
+            { name: 'ember-cli-code-coverage', target: '~0.3.4' },
+            { name: 'ember-cli-template-lint', target: '~0.7.5' },
+          ],
+        });
       });
-    });
-  }
+  },
 };
