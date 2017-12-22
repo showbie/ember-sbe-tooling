2fb118f9ebb5089450868740e989ac81c0125ec9
Jeff <rustyangel@gmail.com>
2017-12-22 11:42:20 -0700

    New: Enable object rest/spread ES features

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,13 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    babel: {
+      plugins: ['transform-object-rest-spread'],
+    },
+
+    'ember-cli-babel': {
+      includePolyfill: true,
+    },
   });
 
   /*
